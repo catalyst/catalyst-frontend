038dd1bf49ddd42c1cd7f2dfd5d546962a76d565
Jen Zajac <jen@catalyst.net.nz>
2017-05-09 17:59:18 +1200

    Fix reconfigure option

--- bin/catalyst-frontend.js
+++ bin/catalyst-frontend.js
@@ -6,6 +6,12 @@ var yeoman = require('yeoman-environment');
 var options = require('yargs').argv;
 var env = yeoman.createEnv();
 
+if (options.reconfigure) {
+  options = { 'reconfigure': true };
+} else {
+  options = {};
+}
+
 env.lookup(function () {
   env.run('catalyst-frontend', options, function (err) {
     if (err) {
