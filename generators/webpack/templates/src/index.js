a3399896d4caf51388b1bf513673893f2194d42f
alexeyomelchenko <alexeyomelchenko@catalyst.net.nz>
2021-02-03 11:45:15 +1300

    fix storybook

--- generators/webpack/templates/src/index.js
+++ generators/webpack/templates/src/index.js
@@ -1,26 +1,7 @@
 <% if (options.react) { %>import React from 'react';
 import { render } from 'react-dom';
-import { AppContainer } from 'react-hot-loader'; // eslint-disable-line import/no-extraneous-dependencies
 import App from 'app';
 
 <% } %>import 'index.scss';<% if (options.react) { %>
 
-const renderComponent = (Component) => {
-  render(
-    <AppContainer>
-      <Component />
-    </AppContainer>,
-    document.getElementById('app')
-  );
-};
-
-renderComponent(App);
-
-// Webpack Hot Module Replacement API
-if (module.hot) {
-  // only ever in dev
-  module.hot.accept('./app', () => {
-    // global-require
-    renderComponent(require('./app').default); // eslint-disable-line
-  });
-}<% } %>
+render(<App />, document.getElementById('app'));<% } %>
