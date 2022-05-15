eb2b16044b18b04a657e598d2a2a3f17226e6c04
Alexey Omelchenko <alexeyomelchenko@alexeyomelchenko-lp.dynamic.wgtn.cat-it.co.nz>
2022-05-16 09:15:59 +1200

    CHORE: library update

--- generators/react-project/templates/src/App.tsx
+++ generators/react-project/templates/src/App.tsx
@@ -5,10 +5,12 @@ import ExampleComponent from 'examples/ExampleComponent/ExampleComponent';
 // if you are integrating redux this is a good place to wrap your app in <Provider store={store}>
 // if you are using react-router this is a good place to set up your router
 
-const App = () => (
-  <div>
-    <ExampleComponent />
-  </div>
-);
+function App() {
+  return (
+    <div>
+      <ExampleComponent />
+    </div>
+  );
+}
 
 export default hot(App);
