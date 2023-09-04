83d1a5ca626f28c01397382eda3e26bb3d74ad4f
Alexey Omelchenko <alexeyomelchenko@catalyst.net.nz>
2023-09-04 13:50:50 +1200

    CHORE: update librares

--- generators/react-project/templates/.stylelintrc.js
+++ generators/react-project/templates/.stylelintrc.js
@@ -3,15 +3,21 @@ module.exports = {
     'stylelint-config-standard',
     'stylelint-config-sass-guidelines'
   ],
+  'plugins': [
+    "stylelint-order"
+  ]
   'defaultSeverity': 'warning',
   'rules': {
+    'import-notation': 'string',
     'font-family-no-missing-generic-family-keyword': null,
     'max-nesting-depth': [3, {
       'ignore': ['blockless-at-rules', 'pseudo-classes'],
       'ignoreAtRules': ['media']
     }],
+    "order/properties-order": [
+      "position"
+    ],
     'no-descending-specificity': null,
-    'order/properties-alphabetical-order': null,
     'selector-max-compound-selectors': [4],
     'selector-max-id': [1],
     'selector-no-qualifying-type': null
